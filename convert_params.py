cab44cbd5543a106c65496094104e4ea427012a7
YBRua <YBiuR0929@163.com>
2021-12-02 13:36:19 +0800

    formatted `convert_params.py`

--- convert_params.py
+++ convert_params.py
@@ -13,7 +13,7 @@ from src.utils.config import cfg
 
 def convert_params(model_th, model_pd, model_path):
     """
-    convert pytorch model's parameters into paddlepaddle model, then saving as .pdparams
+    convert pytorch model's parameters into paddlepaddle model, then save as .pdparams
     :param model_th: pytorch model which has loaded pretrained parameters.
     :param model_pd: paddlepaddle dygraph model
     :param model_path: paddlepaddle dygraph model path
@@ -31,11 +31,11 @@ def convert_params(model_th, model_pd, model_path):
         if key_pd in state_dict_th.keys():
             key_th = key_pd
         # Following tailor to our Graph Match work
-        elif ('gnn_layer_list' in key_pd) :
-            key_th =  key_pd.replace('list.', '')
-        elif ('aff_layer_list' in key_pd) :
+        elif ('gnn_layer_list' in key_pd):
+            key_th = key_pd.replace('list.', '')
+        elif ('aff_layer_list' in key_pd):
             key_th = key_pd.replace('aff_layer_list.', 'affinity_')
-        elif ('cross_layer' in key_pd) :
+        elif ('cross_layer' in key_pd):
             # noww only support **one** cross layer
             key_th = key_pd.replace('cross_layer', 'cross_graph_0')
 
@@ -46,23 +46,29 @@ def convert_params(model_th, model_pd, model_path):
 
         if "fc" in key_th or "classifier":
             if len(state_dict_pd[key_pd].shape) < 4:
-                state_dict[key_pd] = state_dict_th[key_th].numpy().astype("float32").transpose()
+                state_dict[key_pd] = state_dict_th[key_th].numpy().astype(
+                    "float32").transpose()
             else:
-                state_dict[key_pd] = state_dict_th[key_th].numpy().astype("float32")
+                state_dict[key_pd] = state_dict_th[key_th].numpy().astype(
+                    "float32")
         else:
-            state_dict[key_pd] = state_dict_th[key_th].numpy().astype("float32")
+            state_dict[key_pd] = state_dict_th[key_th].numpy().astype(
+                "float32")
 
     assert len(state_dict_pd.keys()) == len(state_dict.keys())
     try:
-        len(state_dict.keys()) + num_batches_tracked_th == len(state_dict_th.keys())
+        len(state_dict.keys()) + \
+            num_batches_tracked_th == len(state_dict_th.keys())
     except Exception as e:
-        print(f"The number of num_batches_tracked is {num_batches_tracked_th} in pytorch model.")
+        print(
+            f"The number of num_batches_tracked is {num_batches_tracked_th} in pytorch model.")
         print("Exception: there are other layer parameter which not converted")
 
     model_pd.set_dict(state_dict)
 
     fluid.dygraph.save_dygraph(model_pd.state_dict(), model_path=model_path)
     print("model convert successfully.")
+
 
 def vgg_convert():
     with fluid.dygraph.guard():
@@ -75,10 +81,11 @@ def vgg_convert():
         print(len(model_pd.state_dict().keys()))
         convert_params(model_th, model_pd, model_path)
 
+
 def pca_convert():
     '''
     If u want to convert PCA
-    please move this file to the father dir
+    please move this file to the parent dir
     '''
     with fluid.dygraph.guard():
         model_th = tchPCA()
@@ -92,12 +99,13 @@ def pca_convert():
         print(model_pd.state_dict().keys())
         print(len(model_pd.state_dict().keys()))
         convert_params(model_th, model_pd, model_path)
-    
+
 
 if __name__ == '__main__':
     from src.utils.dup_stdout_manager import DupStdoutFileManager
     from src.utils.parse_args import parse_args
     from src.utils.print_easydict import print_easydict
 
-    args = parse_args('Deep learning of graph matching training & evaluation code.')
+    args = parse_args(
+        'Deep learning of graph matching training & evaluation code.')
     pca_convert()
