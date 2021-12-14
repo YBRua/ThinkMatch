3a7f1e5415f7dfe0615e88a51f7a627010d1e4bc
YBRua <YBiuR0929@163.com>
2021-12-14 22:26:34 +0800

    fixed misuse in fix of `transpose`

--- models/CIE/model_pdl.py
+++ models/CIE/model_pdl.py
@@ -115,14 +115,14 @@ class Net(CNN):
         emb_edge2 = Q_tgt.unsqueeze(-1)
 
         # adjacency matrices
-        A_src = paddle.bmm(G_src, paddle.transpose(H_src, (1, 2)))
-        A_tgt = paddle.bmm(G_tgt, paddle.transpose(H_tgt, (1, 2)))
+        A_src = paddle.bmm(G_src, paddle.transpose(H_src, (0, 2, 1)))
+        A_tgt = paddle.bmm(G_tgt, paddle.transpose(H_tgt, (0, 2, 1)))
 
         # U_src, F_src are features at different scales
         emb1 = paddle.transpose(
-            paddle.concat((U_src, F_src), axis=1),(1, 2))
+            paddle.concat((U_src, F_src), axis=1), (0, 2, 1))
         emb2 = paddle.transpose(
-            paddle.concat((U_tgt, F_tgt), axis=1), (1, 2))
+            paddle.concat((U_tgt, F_tgt), axis=1), (0, 2, 1))
         ss = []
 
         # emb1, emb2, emb_edge1, emb_edge2 = self.gnn_layer_0(
@@ -168,7 +168,8 @@ class Net(CNN):
                         (emb1, paddle.bmm(s, emb2)), axis=-1))
                 new_emb2 = cross_graph(
                     paddle.concat(
-                        (emb2, paddle.bmm(paddle.transpose(s, [1, 2]), emb1)), axis=-1))
+                        (emb2, paddle.bmm(s.transpose((0, 2, 1)), emb1)),
+                        axis=-1))
                 emb1 = new_emb1
                 emb2 = new_emb2
 
