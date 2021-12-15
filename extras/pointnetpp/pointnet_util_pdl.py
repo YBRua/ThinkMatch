673f967657e4e3ef4a7a75d907e20f35ebbb100e
YBRua <YBiuR0929@163.com>
2021-12-15 22:28:42 +0800

    fix?

--- extras/pointnetpp/pointnet_util_pdl.py
+++ extras/pointnetpp/pointnet_util_pdl.py
@@ -73,7 +73,7 @@ def farthest_point_sample(xyz, npoint):
     centroids = paddle.zeros((B, npoint), dtype='int64')
     distance = paddle.ones((B, N)) * 1e10
     farthest = paddle.randint(0, N, (B,), dtype='int64')
-    batch_indices = paddle.arange(B, dtype='int64').numpy()
+    batch_indices = paddle.arange(B, dtype='int64').tolist()
     for i in range(npoint):
         centroids[:, i] = farthest
         centroid = xyz[batch_indices, farthest, :].reshape((B, 1, 3))
