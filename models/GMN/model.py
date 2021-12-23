fd8fdc50d63fd0d3e768398e2f2cf6351e88a0a8
YBRua <YBiuR0929@163.com>
2021-12-23 17:18:19 +0800

    fixed io signature mismatch

--- models/GMN/model.py
+++ models/GMN/model.py
@@ -20,14 +20,24 @@ class Net(CNN):
         super(Net, self).__init__()
         self.affinity_layer = Affinity(cfg.GMN.FEATURE_CHANNEL)
         self.power_iteration = PowerIteration(max_iter=cfg.GMN.PI_ITER_NUM, stop_thresh=cfg.GMN.PI_STOP_THRESH)
-        self.bi_stochastic = Sinkhorn(max_iter=cfg.GMN.BS_ITER_NUM, epsilon=cfg.GMN.BS_EPSILON)
+        self.bi_stochastic = Sinkhorn(
+            max_iter=cfg.CIE.SK_ITER_NUM, epsilon=cfg.CIE.SK_EPSILON)
         self.voting_layer = Voting(alpha=cfg.GMN.VOTING_ALPHA)
         self.displacement_layer = Displacement()
         self.l2norm = nn.LocalResponseNorm(cfg.GMN.FEATURE_CHANNEL * 2, alpha=cfg.GMN.FEATURE_CHANNEL * 2, beta=0.5, k=0)
+        self.rescale = cfg.PROBLEM.RESCALE
 
-    def forward(self, src, tgt, P_src, P_tgt, G_src, G_tgt, H_src, H_tgt, ns_src, ns_tgt, K_G, K_H, type='img'):
-        if type == 'img' or type == 'image':
-            # extract feature
+    def forward(self, data_dict, **kwargs):
+        if 'image' in data_dict:
+            # real image data
+            src, tgt = data_dict['images']
+            P_src, P_tgt = data_dict['Ps']
+            ns_src, ns_tgt = data_dict['ns']
+            G_src, G_tgt = data_dict['Gs']
+            H_src, H_tgt = data_dict['Hs']
+            K_G, K_H = data_dict['KGHs']
+
+            # extract features
             src_node = self.node_layers(src)
             src_edge = self.edge_layers(src_node)
             tgt_node = self.node_layers(tgt)
@@ -40,11 +50,19 @@ class Net(CNN):
             tgt_edge = self.l2norm(tgt_edge)
 
             # arrange features
-            U_src = feature_align(src_node, P_src, ns_src, cfg.PAIR.RESCALE)
-            F_src = feature_align(src_edge, P_src, ns_src, cfg.PAIR.RESCALE)
-            U_tgt = feature_align(tgt_node, P_tgt, ns_tgt, cfg.PAIR.RESCALE)
-            F_tgt = feature_align(tgt_edge, P_tgt, ns_tgt, cfg.PAIR.RESCALE)
-        elif type == 'feat' or type == 'feature':
+            U_src = feature_align(src_node, P_src, ns_src, self.rescale)
+            F_src = feature_align(src_edge, P_src, ns_src, self.rescale)
+            U_tgt = feature_align(tgt_node, P_tgt, ns_tgt, self.rescale)
+            F_tgt = feature_align(tgt_edge, P_tgt, ns_tgt, self.rescale)
+
+        elif 'feature' in data_dict:
+            # synthetic data
+            src, tgt = data_dict['features']
+            ns_src, ns_tgt = data_dict['ns']
+            G_src, G_tgt = data_dict['Gs']
+            H_src, H_tgt = data_dict['Hs']
+            K_G, K_H = data_dict['KGHs']
+
             U_src = src[:, :src.shape[1] // 2, :]
             F_src = src[:, src.shape[1] // 2:, :]
             U_tgt = tgt[:, :tgt.shape[1] // 2, :]
